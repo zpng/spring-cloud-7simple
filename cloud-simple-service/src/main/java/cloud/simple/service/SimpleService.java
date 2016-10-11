65738e29561c1667e37bc4c5de240da747fc4cb9
zhangpeng <zhangpeng@fenbi.com>
2016-10-11 23:01:45 +0800

    MOD: remove unsue eureka server uri

--- cloud-simple-service/src/main/java/cloud/simple/service/SimpleService.java
+++ cloud-simple-service/src/main/java/cloud/simple/service/SimpleService.java
@@ -8,12 +8,14 @@ package cloud.simple.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-/**
+/*
  * @author zhangpeng
  */
 @RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SimpleService {
     public static void main(String[] args) {
